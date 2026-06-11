acda9720b078cbf4fed5997b5845c535d38292c3
cody norwood <cody.norwood@student.centralia.edu>
2026-06-11 13:19:32 -0700

    Work on when I get home

--- PetApp/MeetupForm.Designer.cs
+++ PetApp/MeetupForm.Designer.cs
@@ -29,37 +29,22 @@
         private void InitializeComponent()
         {
             Btn_CreateRoom = new Button();
-            PetPicture = new PictureBox();
-            pictureBox1 = new PictureBox();
             Btn_JoinRoom = new Button();
-            ((System.ComponentModel.ISupportInitialize)PetPicture).BeginInit();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
             // 
             // Btn_CreateRoom
             // 
-            Btn_CreateRoom.Location = new Point(367, 85);
+            Btn_CreateRoom.Location = new Point(274, 57);
             Btn_CreateRoom.Name = "Btn_CreateRoom";
-            Btn_CreateRoom.Size = new Size(75, 23);
+            Btn_CreateRoom.Size = new Size(283, 23);
             Btn_CreateRoom.TabIndex = 0;
             Btn_CreateRoom.Text = "Create Room";
             Btn_CreateRoom.UseVisualStyleBackColor = true;
-            // 
-            // PetPicture
-            // 
-            PetPicture.Location = new Point(9, 98);
-            PetPicture.Name = "PetPicture";
-            PetPicture.Size = new Size(352, 340);
-            PetPicture.TabIndex = 1;
-            PetPicture.TabStop = false;
-            // 
-            // pictureBox1
-            // 
-            pictureBox1.Location = new Point(448, 98);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(352, 340);
-            pictureBox1.TabIndex = 2;
-            pictureBox1.TabStop = false;
+            Btn_CreateRoom.Click += Btn_CreateRoom_Click;
             // 
             // Btn_JoinRoom
             // 
@@ -70,29 +55,47 @@
             Btn_JoinRoom.Text = "Join Room";
             Btn_JoinRoom.UseVisualStyleBackColor = true;
             // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(12, 160);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(349, 278);
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(448, 160);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(340, 278);
+            pictureBox2.TabIndex = 5;
+            pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox2_Click;
+            // 
             // MeetupForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.InactiveCaption;
             ClientSize = new Size(800, 450);
-            Controls.Add(Btn_JoinRoom);
+            Controls.Add(pictureBox2);
             Controls.Add(pictureBox1);
-            Controls.Add(PetPicture);
+            Controls.Add(Btn_JoinRoom);
             Controls.Add(Btn_CreateRoom);
             Name = "MeetupForm";
             Text = "MeetupForm";
             Load += MeetupForm_Load;
-            ((System.ComponentModel.ISupportInitialize)PetPicture).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
         public Button Btn_CreateRoom;
-        public PictureBox PetPicture;
-        public PictureBox pictureBox1;
         public Button Btn_JoinRoom;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
     }
 }
--- PetApp/MeetupForm.cs
+++ PetApp/MeetupForm.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,12 +13,37 @@ namespace PetApp
 {
     public partial class MeetupForm : Form
     {
+        static HttpClient client = new HttpClient();
+        const string api_key = "?api-key=foo";
+        Image petImage;
         public MeetupForm()
         {
+
             InitializeComponent();
+            client.BaseAddress = new Uri("https://virtualpetparty.up.railway.app");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            this.petImage = Properties.Resources.HappyDog;
+            this.pictureBox2.Image = Properties.Resources.SadDog;
+
         }
 
         private void MeetupForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_CreateRoom_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
         {
 
         }
